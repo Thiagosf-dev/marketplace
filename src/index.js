3d7b43e5db149346c54557d2b3831b4f048307c7
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2019-08-09 22:55:45 -0400

    commmit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 const port = require('./config/config').port
 
-server.listen(port, () => {
+server.listen(process.env.port || 3000, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
 })
