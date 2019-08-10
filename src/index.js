3408cadfc5f20b2e867cd0e494a091e730512ac8
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2019-08-09 23:01:37 -0400

    alterando a PORT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 const port = require('./config/config').port
 
-server.listen(process.env.port || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
 })
