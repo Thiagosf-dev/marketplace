3bc1520686d14672f588bdb568adacdbb6a300de
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2019-08-09 22:53:56 -0400

    configurando porta padrão

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000 || process.env.port,
+  port: process.env.port || 3000,
   urlConnection: process.env.URL_CONNECTION,
   secretKey: process.env.APP_SECRET,
   ttl: process.env.APP_TTL,
